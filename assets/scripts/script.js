8d4a2e56d87f2f12b1ce946b115d09dd40f45636
Andrew <andrew.ross4224@gmail.com>
2026-05-18 17:59:33 -0400

    lodging page done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,3 @@
-window.addEventListener('resize', (event) => {
-    if (window.innerWidth >= 700) {
-        var inactiveLinks = document.getElementsByClassName("inactive-pages");
-        for (let element of inactiveLinks) {
-            element.style.display = "block";
-            element.style.padding = "5px 0px 0px 20px";
-        }
-    } else {
-        var inactiveLinks = document.getElementsByClassName("inactive-pages");
-        for (let element of inactiveLinks) {
-            element.style.display = "none";
-            element.style.padding = "0";
-        }
-    }
-});
-
 function openNav() {
     var inactiveLinks = document.getElementsByClassName("inactive-pages");
     for (let element of inactiveLinks) {
